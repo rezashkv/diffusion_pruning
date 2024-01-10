c101dcaf570a15eb25738349a3efa69a2e83b645
Your Correct Name <rezashkv@umd.edu>
2024-01-09 21:06:11 -0500

    fix resource loss bug

--- pdm/training/trainer.py
+++ pdm/training/trainer.py
@@ -536,7 +536,7 @@ class DiffPruningTrainer:
                 if global_step == 0:
                     p = self.config.training.losses.resource_loss.pruning_target
                     p_actual = (1 - (1 - p) * self.unet.resource_info_dict['total_flops'] /
-                                self.unet.resource_info_dict['cur_prunable_flops'])
+                                self.unet.resource_info_dict['cur_prunable_flops']).item()
                     self.resource_loss.p = p_actual
 
                 curr_flops = flops_dict['cur_prunable_flops'].mean()
