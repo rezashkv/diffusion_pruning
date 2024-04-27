d7d4e1d53339a085e299895837b6555d130c8104
Your Correct Name <rezashkv@umd.edu>
2024-04-27 16:26:19 -0400

    update fid

--- scripts/fid.py
+++ scripts/fid.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+
 from cleanfid import fid
 import logging
 
@@ -10,13 +12,20 @@ def parse_args():
     parser.add_argument('--gen_dir', type=str, required=True)
     parser.add_argument('--dataset', type=str, default="coco-30k")
     parser.add_argument('--mode', type=str, default="legacy_pytorch")
+    parser.add_argument('--result_dir', type=str, required=True, help="Directory to save the results")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     fid_value = fid.compute_fid(args.ref_dir, dataset_name=args.dataset, mode=args.mode, dataset_split="custom")
-    logging.info(f"FID value: {fid_value}")
+    logging.info(f"FID: {fid_value}")
+
+    os.makedirs(args.result_dir, exist_ok=True)
+    #append to file
+    with open(f"{args.result_dir}/fid.txt", "a") as f:
+        f.write(f"{args.gen_dir} {fid_value}\n")
+
 
 if __name__ == '__main__':
     main()
