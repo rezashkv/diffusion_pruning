4e57b15d5d0c6c23a0dc4736a5ac8ff774efd1a5
Your Correct Name <rezashkv@umd.edu>
2024-04-27 16:23:55 -0400

    update fid

--- scripts/fid.py
+++ scripts/fid.py
@@ -1,7 +1,4 @@
 import argparse
-import os
-
-from PIL import Image
 from cleanfid import fid
 import logging
 
@@ -11,21 +8,14 @@ logging.basicConfig(level=logging.INFO)
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gen_dir', type=str, required=True)
-    parser.add_argument('--dataset', type=str, required=True)
-    parser.add_argument('--model', type=str, default="legacy_pytorch")
+    parser.add_argument('--dataset', type=str, default="coco-30k")
+    parser.add_argument('--mode', type=str, default="legacy_pytorch")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    # iterate over ref_dir and delete corrupted images
-    for file in os.listdir(args.ref_dir):
-        try:
-            Image.open(os.path.join(args.ref_dir, file))
-        except:
-            os.remove(os.path.join(args.ref_dir, file))
-
-    fid_value = fid.compute_fid(args.ref_dir, args.gen_dir, mode=args.mode)
+    fid_value = fid.compute_fid(args.ref_dir, dataset_name=args.dataset, mode=args.mode, dataset_split="custom")
     logging.info(f"FID value: {fid_value}")
 
 if __name__ == '__main__':
