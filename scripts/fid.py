30b2862e401c21b69879bedda3078c98739cd422
Your Correct Name <rezashkv@umd.edu>
2024-04-27 16:22:29 -0400

    update fid

--- scripts/fid.py
+++ scripts/fid.py
@@ -3,12 +3,16 @@ import os
 
 from PIL import Image
 from cleanfid import fid
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gen_dir', type=str, required=True)
-    parser.add_argument('--ref_dir', type=str, required=True)
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--model', type=str, default="legacy_pytorch")
     return parser.parse_args()
 
 
@@ -21,9 +25,8 @@ def main():
         except:
             os.remove(os.path.join(args.ref_dir, file))
 
-    fid_value = fid.compute_fid(args.ref_dir, args.gen_dir, mode="clean")
-    print('FID: ', fid_value)
-
+    fid_value = fid.compute_fid(args.ref_dir, args.gen_dir, mode=args.mode)
+    logging.info(f"FID value: {fid_value}")
 
 if __name__ == '__main__':
     main()
