ba87d1aeca9996b2824cdde077b3b4c6d92ce053
Your Correct Name <rezashkv@umd.edu>
2024-05-08 19:31:26 -0400

    update clip score

--- scripts/clip_score.py
+++ scripts/clip_score.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=logging.INFO)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gen_dir', type=str, required=True)
-    parser.add_argument('--text_features_path', type=str, required=True)
+    parser.add_argument('--gen_images_dir', type=str, required=True)
+    parser.add_argument('--text_features_dir', type=str, required=True)
     parser.add_argument('--clip_model', type=str, default="ViT-B/32")
     parser.add_argument('--num_workers', type=int, default=None)
     parser.add_argument('--batch_size', type=int, default=64)
@@ -20,14 +20,14 @@ def parse_args():
 
 def main():
     args = parse_args()
-    score = clip_score(args.text_features_path, args.gen_dir, clip_model=args.clip_model, num_workers=args.num_workers,
+    score = clip_score(args.text_features_dir, args.gen_images_dir, clip_model=args.clip_model, num_workers=args.num_workers,
                        batch_size=args.batch_size)
     logging.info(f"CLIP score: {score}")
 
     os.makedirs(args.result_dir, exist_ok=True)
 
     with open(f"{args.result_dir}/clip_score_{args.dataset_name}.txt", "a") as f:
-        f.write(f"{args.gen_dir} {score}\n")
+        f.write(f"{args.gen_images_dir} {score}\n")
 
 
 if __name__ == '__main__':
