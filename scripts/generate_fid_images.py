0ea3a93b6e8b3fea5a772abd0d28ff7a2e283660
Your Correct Name <rezashkv@umd.edu>
2024-05-09 11:03:05 -0400

    update fid generating

--- scripts/generate_fid_images.py
+++ scripts/generate_fid_images.py
@@ -155,10 +155,11 @@ def main():
                 collate_fn=collate_fn
             )
 
+            dataloader = accelerator.prepare(dataloader)
+
         else:
             raise ValueError(f"Dataset {data_dir} not supported.")
 
-    dataloader = accelerator.prepare(dataloader)
 
     # #################################################### Models ####################################################
     arch_v = torch.load(os.path.join(config.finetuning_ckpt_dir, "arch_vector.pt"), map_location="cpu")
