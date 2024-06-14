b475be325e59f6183956c1c81a2082cc598d8a66
Your Correct Name <rezashkv@umd.edu>
2024-06-14 11:59:42 -0400

    udpate models init

--- pdm/models/__init__.py
+++ pdm/models/__init__.py
@@ -1,3 +1,3 @@
-from .unet.unet_2d_conditional import UNet2DConditionModelGated
+from .unet.unet_2d_conditional import UNet2DConditionModelGated, UNet2DConditionModelPruned
 from .hypernet.hypernet import HyperStructure
 from .vq.quantizer import StructureVectorQuantizer
